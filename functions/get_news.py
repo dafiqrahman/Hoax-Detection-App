05fb96ae1f2752b760427b5d1d6c8231662953e1
dafiqrahman <46261435+dafiqrahman@users.noreply.github.com>
2023-09-02 19:31:34 +0700

    limit length on search (#9)

--- functions/get_news.py
+++ functions/get_news.py
@@ -10,6 +10,8 @@ def make_clickable(link,text):
 def get_news(content,prediksi,API_SEARCH_TOKEN,CSE_TOKEN):
  #import library request
     QUERY = content
+    if len(QUERY.split()) > 10:
+        QUERY = ' '.join(QUERY.split()[:10])
     results = requests.get(f"https://www.googleapis.com/customsearch/v1?key={API_SEARCH_TOKEN}&cx={CSE_TOKEN}&q={QUERY}").json()['items']
     title = [result['title'] for result in results]
     link = [result['link'] for result in results]
