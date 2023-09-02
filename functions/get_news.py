1e44e7ad643a2ac040898e92810dab5870769c35
Dafiq <vabio.costa@gmail.com>
2023-09-02 16:56:37 +0700

    add model to app

--- functions/get_news.py
+++ functions/get_news.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from googlesearch import search
 
 def make_clickable(link,text):
     # target _blank to open new window
@@ -8,19 +8,22 @@ def make_clickable(link,text):
     return f'<a target="_blank" href="{link}">{text}</a>'
 
 def get_news(content,prediksi):
-    link = ["https://stackoverflow.com/questions/71641666/hyperlink-in-streamlit-dataframe",
-            "https://stackoverflow.com/questions/71731937/how-to-plot-comparison-in-streamlit-dynamically-with-multiselect"]
-    text = ["Hyperlink in Streamlit dataframe",
-            "How to plot comparison in Streamlit dynamically with multiselect?"]
-    if prediksi == 'Hoax':
+ 
+    results = search(content,num_results=5,lang="id",advanced=True)
+    results = list(results)
+    link = [result.url for result in results]
+    text = [result.title for result in results]
+    
+    if prediksi == 'Hoaks':
         prediksi = 'Kontradiksi'
     else:
         prediksi = 'Selaras'
+    link_berita = [make_clickable(l,t) for l,t in zip(link,text)]
+    isi = [prediksi for i in range(len(link_berita))]
     df = pd.DataFrame(
         {
-            "Link Berita": [make_clickable(link[0],text[0]),
-                    make_clickable(link[1],text[1])],
-            "Isi": [prediksi, prediksi]
+            "Link Berita": link_berita,
+            "Isi": isi,
         }
     )
     return df
--- hoax_app.py
+++ hoax_app.py
@@ -6,11 +6,13 @@ from functions.get_news import get_news
 #streamlit makes 2 columns
 # st.set_page_config(layout="wide")
 
+
 st.title('Hoax Detection App')
-st.write('This app is used to detect hoax news based on the title and the content of the news. The model used is a combination of LSTM and CNN. The dataset used is the Fake and Real News Dataset from Kaggle.')
-st.header('Input Content')
+st.write('Aplikasi ini digunakan untuk mendeteksi berita hoax berdasarkan isi berita. Model yang digunakan adalah DistilBERT. Dataset yang digunakan adalah Dataset Berita Palsu dan Nyata dari Kaggle dan Jurnal terkait.')
+st.header('Input Berita')
 col1, col2 = st.columns(2)
 
+API_TOKEN = st.secrets["API_TOKEN"]
 #input
 with col1:
     konten = st.text_area('Masukkan isi konten yang ingin dideteksi')
@@ -21,7 +23,7 @@ with col2:
     st.write("  ")
     sub_col1, sub_col2,sub_col3 = st.columns(3)
     if deteksi_button:
-        hoax, valid, prediksi = deteksi(konten)
+        hoax, valid, prediksi = deteksi(konten,API_TOKEN)
         sub_col1.metric(label='Hoax', value=hoax)
         sub_col2.metric(label='Valid', value=valid)
         sub_col3.metric(label='Prediksi', value=prediksi)
